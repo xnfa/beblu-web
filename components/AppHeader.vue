4bda7f6b0748f97a29ec0112e27869316ebbe144
xnfa <xnfa2000@gmail.com>
2022-01-31 03:37:16 +0800

    ...

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,7 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <Disclosure as="nav" class="bg-white shadow-[0_1px_0_#E7E7E7] relative bg-whit z-10" v-slot="{ open }">
-    <div class="container mx-auto px-6 lg:px-8 bg-white z-10">
+  <Disclosure as="nav" class="bg-white shadow-[0_1px_0_#E7E7E7] relative bg-whit z-10" v-slot="{ open, close }">
+    <div class="relative container mx-auto px-6 lg:px-8 bg-white z-10">
       <div class="relative flex items-center justify-between bg-white h-16">
         <transition
           enter-active-class="transition ease-out duration-300"
@@ -11,31 +11,33 @@
           leave-from-class="transform opacity-100 scale-100"
           leave-to-class="transform opacity-0 scale-0"
         >
-          <div v-if="!open" class="absolute inset-y-0 right-0 flex items-center sm:hidden">
+          <div v-if="!open" class="absolute inset-y-0 right-0 flex items-center md:hidden">
             <!-- Mobile menu button-->
             <DisclosureButton @click="openMenu" class="inline-flex items-center justify-center p-2 rounded-md text-black">
               <span class="sr-only">Open main menu</span>
               <img src="/images/icons/close.svg" class="block h-6 w-6" alt="">
             </DisclosureButton>
           </div>
-          <div v-else class="absolute inset-y-0 right-0 flex items-center sm:hidden">
+          <div v-else class="absolute inset-y-0 right-0 flex items-center md:hidden">
             <!-- Mobile menu button-->
-            <DisclosureButton  @click="closeMenu" class="inline-flex items-center justify-center p-2 rounded-md text-black">
+            <DisclosureButton @click="closeMenu" class="inline-flex items-center justify-center p-2 rounded-md text-black">
               <span class="sr-only">Open main menu</span>
               <XIcon class="block h-6 w-6" aria-hidden="true" />
             </DisclosureButton>
           </div>
         </transition>
         <div class="flex-1 flex items-center justify-start sm:items-stretch sm:justify-start">
-          <img class="block h-7 w-auto" src="/images/logo.svg" alt="Workflow" />
-          <div class="hidden items-center justify-center sm:flex sm:ml-6 flex-1">
+          <NuxtLink to="/" class="inline-block" @click="close(); closeMenu()">
+            <img class="block h-7 w-auto" src="/images/logo.svg" alt="Workflow" @click=""/>
+          </NuxtLink>
+          <div class="hidden items-center justify-center md:flex md:ml-6 flex-1">
             <div class="space-x-8 text-normal">
-              <NuxtLink to="/download" class="inline-block hover:bg-[#ECFCFE] h-8 w-20 px-2 py-1 rounded-md">
+              <NuxtLink to="/download" class="inline-block hover:bg-[#ECFCFE] h-8 px-2 py-1 rounded-md">
                 Our Story
               </NuxtLink>
               <Disclosure v-slot="{ open }">
                 <DisclosureButton
-                  class="inline-flex relative items-center hover:bg-[#ECFCFE] h-8 w-20 px-2 py-1 rounded-md box-border"
+                  class="inline-flex relative items-center hover:bg-[#ECFCFE] h-8 px-2 py-1 rounded-md box-border"
                 >
                   <span>Solutions</span>
                   <ChevronUpIcon
@@ -52,13 +54,13 @@
                   </DisclosurePanel>
                 </DisclosureButton>
               </Disclosure>
-              <NuxtLink to="/download">
+              <NuxtLink to="/download" class="inline-block hover:bg-[#ECFCFE] h-8 px-2 py-1 rounded-md">
                 Portfolio
               </NuxtLink>
-              <NuxtLink to="/download">
+              <NuxtLink to="/download" class="inline-block hover:bg-[#ECFCFE] h-8 px-2 py-1 rounded-md">
                 Community
               </NuxtLink>
-              <NuxtLink to="/download">
+              <NuxtLink to="/download" class="inline-block hover:bg-[#ECFCFE] h-8 px-2 py-1 rounded-md">
                 Contact us
               </NuxtLink>
               <!-- <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-normal" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a> -->
@@ -92,35 +94,35 @@
     </div>
 
     <transition
-      enter-active-class="transition duration-850 ease-out"
-      enter-from-class="transform opacity-0 -translate-y-full"
-      enter-to-class="transform opacity-150 translate-y-0"
-      leave-active-class="transition duration-850 ease-out"
-      leave-from-class="transform opacity-100 translate-y-0"
-      leave-to-class="transform opacity-0 -translate-y-full"
+      enter-active-class="transition duration-500 ease-out"
+      enter-from-class="transform -translate-y-full"
+      enter-to-class="transform translate-y-0"
+      leave-active-class="transition duration-500 ease-out"
+      leave-from-class="transform translate-y-0"
+      leave-to-class="transform -translate-y-full"
     >
-      <DisclosurePanel class="absolute w-full bg-[#F4F6FA] z-0 h-[100vh]">
+      <DisclosurePanel class="absolute w-full bg-[#F4F6FA] z-0 h-[100vh]" v-slot="{ close }">
         <div class="px-2 pt-[5.375rem] pb-3 flex flex-col">
           <!-- <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton> -->
-          <NuxtLink to="/download" class="text-[2rem] font-light text-center mb-[2.625rem] rounded-md leading-none">
+          <NuxtLink to="/download" class="text-[2rem] font-light text-center p-5 rounded-md leading-none" @click="close(); closeMenu()">
             Our Story
           </NuxtLink>
-          <NuxtLink to="/download" class="text-[2rem] font-light text-center mb-[2.625rem] rounded-md leading-none">
+          <NuxtLink to="/download" class="text-[2rem] font-light text-center p-5 rounded-md leading-none" @click="close(); closeMenu()">
             Beblu Suite
           </NuxtLink>
-          <NuxtLink to="/download" class="text-[2rem] font-light text-center mb-[2.625rem] rounded-md leading-none">
+          <NuxtLink to="/download" class="text-[2rem] font-light text-center p-5 rounded-md leading-none" @click="close(); closeMenu()">
             Smart Sensors
           </NuxtLink>
-          <NuxtLink to="/download" class="text-[2rem] font-light text-center mb-[2.625rem] rounded-md leading-none">
+          <NuxtLink to="/download" class="text-[2rem] font-light text-center p-5 rounded-md leading-none" @click="close(); closeMenu()">
             Portfolio
           </NuxtLink>
-          <NuxtLink to="/download" class="text-[2rem] font-light text-center mb-[2.625rem] rounded-md leading-none">
+          <NuxtLink to="/download" class="text-[2rem] font-light text-center p-5 rounded-md leading-none" @click="close(); closeMenu()">
             Community
           </NuxtLink>
-          <NuxtLink to="/download" class="text-[2rem] font-light text-center rounded-md leading-none">
+          <NuxtLink to="/download" class="text-[2rem] font-light text-center p-5 rounded-md leading-none" @click="close(); closeMenu()">
             Contact us
           </NuxtLink>
-          <div class="flex items-center justify-center mt-[5rem]">
+          <div class="flex items-center justify-center mt-[4rem]">
             <button class="bg-[#E4E9ED] flex text-xl rounded-full items-center justify-center p-2 space-x-1 leading-none">
               <img src="/images/icons/global.svg" alt="">
               <span>English</span>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <section class="hero flex flex-col items-center h-[40.5rem] relative">
-      <img class="absolute top-0 left-0" src="/images/sections/home-section-1/01.svg" alt="">
+      <!-- <img class="absolute top-0 left-0" src="/images/sections/home-section-1/01.svg" alt="">
       <img class="absolute top-0 left-0" src="/images/sections/home-section-1/02.svg" alt="">
       <img class="absolute top-0 left-0" src="/images/sections/home-section-1/03.svg" alt="">
       <img class="absolute top-0 left-0" src="/images/sections/home-section-1/04.svg" alt="">
       <img class="absolute top-0 left-0" src="/images/sections/home-section-1/05.svg" alt="">
-      <img class="absolute top-0 left-0" src="/images/sections/home-section-1/06.svg" alt="">
+      <img class="absolute top-0 left-0" src="/images/sections/home-section-1/06.svg" alt=""> -->
       <img class="absolute w-[310px] bottom-0" src="/images/sections/home-section-1/screenshot.png" alt="">
       <div class="text-[4.9rem] mt-[112px] font-bold text-normal leading-none">
         One App is Enough
@@ -15,11 +15,11 @@
         AI for your workspace
       </div>
     </section>
-    <section class="container mx-auto h-[25rem] relative">
+    <!-- <section class="container mx-auto h-[25rem] relative">
       <img class="absolute top-0 left-0" src="/images/sections/home-section-2/ball-01.svg" alt="">
       <img class="absolute top-0 left-0" src="/images/sections/home-section-2/ball-02.svg" alt="">
       <img class="absolute top-0 left-0" src="/images/sections/home-section-2/ball-03.svg" alt="">
-    </section>
+    </section> -->
   </div>
 </template>
 
