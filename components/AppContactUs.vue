46f8e9ee5dc267661f6c8a30825e06bd5ee0b69b
xnfa <xnfa2000@gmail.com>
2022-02-11 09:25:14 +0800

    ...

--- components/AppContactUs.vue
+++ components/AppContactUs.vue
@@ -10,25 +10,25 @@
         type="text"
         name="name"
         :placeholder="l('contact_us_name')"
-        class="mb-4 block w-full px-3 py-2 bg-white border border-[#EBEDF0] rounded-md text-sm shadow-sm placeholder-[#BFBFBF] focus:outline-none focus:border-[#37C0BA] focus:ring-1 focus:ring-[#37C0BA] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+        class="mb-4 block w-full px-4 py-[0.8rem] bg-white border border-[#EBEDF0] rounded-md shadow-sm placeholder-[#BFBFBF] focus:outline-none focus:border-[#37C0BA] focus:ring-1 focus:ring-[#37C0BA] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
       />
       <input
         type="text"
         name="company"
         :placeholder="l('contact_us_company')"
-        class="mb-4 block w-full px-3 py-2 bg-white border border-[#EBEDF0] rounded-md text-sm shadow-sm placeholder-[#BFBFBF] focus:outline-none focus:border-[#37C0BA] focus:ring-1 focus:ring-[#37C0BA] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+        class="mb-4 block w-full px-4 py-[0.8rem] bg-white border border-[#EBEDF0] rounded-md shadow-sm placeholder-[#BFBFBF] focus:outline-none focus:border-[#37C0BA] focus:ring-1 focus:ring-[#37C0BA] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
       />
       <input
         type="email"
         name="email"
         :placeholder="l('contact_us_email')"
-        class="mb-4 block w-full px-3 py-2 bg-white border border-[#EBEDF0] rounded-md text-sm shadow-sm placeholder-[#BFBFBF] focus:outline-none focus:border-[#37C0BA] focus:ring-1 focus:ring-[#37C0BA] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+        class="mb-4 block w-full px-4 py-[0.8rem] bg-white border border-[#EBEDF0] rounded-md shadow-sm placeholder-[#BFBFBF] focus:outline-none focus:border-[#37C0BA] focus:ring-1 focus:ring-[#37C0BA] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
       />
       <input
         type="text"
         name="phone"
         :placeholder="l('contact_us_phone')"
-        class="mb-4 block w-full px-3 py-2 bg-white border border-[#EBEDF0] rounded-md text-sm shadow-sm placeholder-[#BFBFBF] focus:outline-none focus:border-[#37C0BA] focus:ring-1 focus:ring-[#37C0BA] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+        class="mb-4 block w-full px-4 py-[0.8rem] bg-white border border-[#EBEDF0] rounded-md shadow-sm placeholder-[#BFBFBF] focus:outline-none focus:border-[#37C0BA] focus:ring-1 focus:ring-[#37C0BA] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
       />
       <textarea
         name="message"
@@ -36,7 +36,7 @@
         cols="30"
         rows="5"
         :placeholder="l('contact_us_message')"
-        class="mb-4 block w-full px-3 py-2 bg-white border border-[#EBEDF0] rounded-md text-sm shadow-sm placeholder-[#BFBFBF] focus:outline-none focus:border-[#37C0BA] focus:ring-1 focus:ring-[#37C0BA] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+        class="mb-4 block w-full px-4 py-[0.8rem] bg-white border border-[#EBEDF0] rounded-md shadow-sm placeholder-[#BFBFBF] focus:outline-none focus:border-[#37C0BA] focus:ring-1 focus:ring-[#37C0BA] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
       ></textarea>
       <p class="text-[#37C0BA] text-xs">
         {{ l("contact_us_required") }}
