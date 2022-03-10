c34d23e43ee45467c843d171ce3361dc6cf3b2ae
xnfa <xnfa2000@gmail.com>
2022-03-10 17:04:03 +0800

    ...

--- pages/_404.vue
+++ pages/_404.vue
@@ -2,8 +2,10 @@
   <app-not-found></app-not-found>
 </template>
 <script setup>
-  const { ssrContext } = useNuxtApp();
-  ssrContext?.nuxt?.error = {
+const { ssrContext } = useNuxtApp();
+if (ssrContext?.nuxt) {
+  ssrContext.nuxt.error = {
     statusCode: 404,
   };
+}
 </script>
